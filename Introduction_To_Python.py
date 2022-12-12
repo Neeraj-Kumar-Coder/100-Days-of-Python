e48e3cde791f45e04839fb39840a688ff30ebdfb
Neeraj Kumar <nk7075034@gmail.com>
2022-12-12 16:54:29 +0530

    Day 1: Learned the basics and about the inventer of python. Also how it was named as 'PYTHON'

---
+++ Introduction_To_Python.py
@@ -0,0 +1,6 @@
+print("Started the 100 Days of code challenge with python")
+print("Python in an Interpreted programming language")
+print("It is an object oriented programming language")
+
+print("\t~invented by Guido van Rossum")
+print("He named it by watching the show 'Monty Python'")
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 100-Days-of-Python
+Target is to learn python from scratch in 100 Days of coding in python.
+
+## Days Target
+#### Day 001: Introduction to Python
