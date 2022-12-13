f8a4eb77e24e0779f320960ec2fc8e2da83fde5b
Neeraj Kumar <nk7075034@gmail.com>
2022-12-13 11:45:35 +0530

    Learned about basics of modules and how to install them using 'pip'

---
+++ Modules_In_Python.py
@@ -0,0 +1,4 @@
+print("A Python module is a file containing Python definitions and statements.\n")
+print("Modules are of two types in python:")
+print("1. Built-in Modules: Modules that are shipped with python")
+print("2. External Modules: Modules written by other users and need to be installed through 'pip' before using")
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Target is to learn python from scratch in 100 Days of coding in python.
 
 ## Days Target
 #### Day 001: Introduction to Python
+#### Day 002: Introduction about Modules in Python
