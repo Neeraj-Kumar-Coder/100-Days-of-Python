75341d7aad86c3756f1da13b4a26287cd7aa454a
Neeraj Kumar <nk7075034@gmail.com>
2023-02-04 16:20:50 +0530

    Introduction to OOPs

---
+++ Introduction_To_OOPs.py
@@ -0,0 +1,7 @@
+# OOPs: Object Oriented Programming style
+
+# Features:
+# 1. Encapsulation: It refers to the bundling of data with the mechanisms or methods that operate on the data, or the limiting of direct access to some data, such as an object's components.
+# 2. Inheritance: It is a feature or a process in which, new classes are created from the existing classes.
+# 3. Abstraction: It is the process of hiding the internal details of an application from the outer world.
+# 4. Polymorphism: It is one of the core concepts of object-oriented programming (OOP) and describes situations in which something occurs in several different forms.
--- README.md
+++ README.md
@@ -56,3 +56,4 @@ Target is to learn python from scratch in 100 Days of coding in python.
 - #### Day 052: Learned about map(), filter() and reduce()
 - #### Day 053: Learned the difference between 'is' and '==' comparison operators
 - #### Day 054: Created a SNAKE, WATER and GUN game using the concepts learned so far
+- #### Day 055: Introduction to OOPs
